26d3359010b408670fabef19636a7af1df9a4900
Adrian Freja D <afrejad@gmail.com>
2026-06-01 20:54:18 -0500

    ajustar rutas para usar en el dominio www.coloquiolenguajeycultura.org

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  site: 'https://afrejad.github.io',
-  base: '/xiii-coloquio-lenguaje-cultura',
+  site: "https://coloquiolenguajeycultura.org"
 });
