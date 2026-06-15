ec9f19b5a6a8a90327c2d779c5a3c8c707cd1cfa
Adrian Freja D <afrejad@gmail.com>
2026-06-15 08:31:06 -0500

    agrega configuración SEO, sitemap y robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,7 @@
 import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
-  site: "https://coloquiolenguajeycultura.org"
+  site: "https://coloquiolenguajeycultura.org",
+  integrations: [sitemap()],
 });
